c219f937ef86ad80abba58b166db5d8a036a79f9
Johnny <johnny.bulinski@gmail.com>
2025-12-18 14:17:38 +0100

    Dodałem plik model.py, train.py

--- src/model.py
+++ src/model.py
@@ -1,18 +1,24 @@
 import torch
 import torch.nn as nn
 
-
-class NeuralNetwork(nn.Module):
-    
-    def __init__(self) -> None:
-        super(NeuralNetwork, self).__init__()
+class CaloriePredictor(nn.Module):
+    def __init__(self, input_dim=7, hidden_dim=64, dropout_rate=0.0):
+        super(CaloriePredictor, self).__init__()
+        
         self.net = nn.Sequential(
-            nn.Linear(7, 8),
+            # Warstwa wejściowa -> Ukryta 1
+            nn.Linear(input_dim, hidden_dim),
             nn.ReLU(),
-            nn.Linear(8, 4),
+            nn.Dropout(dropout_rate), # Opcjonalny Dropout
+            
+            # Warstwa Ukryta 1 -> Ukryta 2
+            nn.Linear(hidden_dim, hidden_dim // 2),
             nn.ReLU(),
-            nn.Linear(4, 1)
+            nn.Dropout(dropout_rate), # Opcjonalny Dropout
+            
+            # Warstwa wyjściowa (1 neuron - przewidywana liczba kalorii)
+            nn.Linear(hidden_dim // 2, 1)
         )
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x):
         return self.net(x)
